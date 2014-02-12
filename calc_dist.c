9adf655d318ece8b950a592953d3c8856c18d7c0
jonathanking <jonathanking@berkeley.edu>
2014-02-12 09:23:12 -0800

    implemented v1 of calc_min_dist. It doesn't take into account anything other than non-translated/fliped/rotated templates. more for loops will be necessary in order to cover every case, store all of the euclidean distances (in an array), and then find the minimum distance within that set.

--- calc_dist.c
+++ calc_dist.c
@@ -14,34 +14,52 @@
 #include "limits.h"
 
 /* Swaps the values pointed to by the pointers X and Y. */
-void swap(unsigned char *x, unsigned char *y) {
+void swap(unsigned char *x, unsigned char *y)
+{
     /* Optional function */
 }
 
 /* Flips the elements of a square array ARR across the y-axis. */
-void flip_horizontal(unsigned char *arr, int width) {
+void flip_horizontal(unsigned char *arr, int width)
+{
     /* Optional function */
 }
 
 /* Transposes the square array ARR. */
-void transpose(unsigned char *arr, int width) {
+void transpose(unsigned char *arr, int width)
+{
     /* Optional function */
 }
 
 /* Rotates the square array ARR by 90 degrees counterclockwise. */
-void rotate_ccw_90(unsigned char *arr, int width) {
+void rotate_ccw_90(unsigned char *arr, int width)
+{
     /* Optional function */
 }
 
 /* Returns the squared Euclidean distance between TEMPLATE and IMAGE. The size of IMAGE
  * is I_WIDTH * I_HEIGHT, while TEMPLATE is square with side length T_WIDTH. The template
  * image should be flipped, rotated, and translated across IMAGE.
+ * 
+ * Returns the smallest distance between IMAGE and all the possible
+ * translations/rotations/flips of TEMPLATE.
  */
-unsigned int calc_min_dist(unsigned char *image, int i_width, int i_height, 
-        unsigned char *template, int t_width) {
+unsigned int calc_min_dist(unsigned char *image, int i_width, int i_height,
+                           unsigned char *template, int t_width)
+{
     unsigned int min_dist = UINT_MAX;
-    /* YOUR CODE HERE */
     //implement Euclidean distance formula, obtain repsective pixel values for each
-    // ex (0,0) a vs (0,0)b ---> (0,0)a - (0,0)b
-    return min_dist;
-}   
+    // ex (0,0) a vs (0,0)b ---> ((0,0)a - (0,0)b)^2
+    // a and b represent test image, template image, respectively
+
+    int MN = i_width * i_height;
+    int temp_size = t_width * t_width;
+    int e_distance = 0;
+
+    for (int p = 0; p < temp_size; p++) { // this loop assumes both images are square, though this will not be true for cases other than the extra credit
+        char a_val = image[p];
+        char b_val = image[p];
+        e_distance += (a_val - b_val) * (a_val - b_val);
+    }
+    return e_distance;
+}
