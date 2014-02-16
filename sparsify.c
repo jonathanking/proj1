cce6b867521c8fae442b23164e481eb391c05b56
jonathanking <jonathanking@berkeley.edu>
2014-02-15 17:13:47 -0800

    added tests to sparsify.

--- sparsify.c
+++ sparsify.c
@@ -1,6 +1,6 @@
 /* CS61C Sp14 Project 1
- * 
- * Converts a bitmap array into a sparse representation of the matrix. 
+ *
+ * Converts a bitmap array into a sparse representation of the matrix.
  *
  * ANY MODIFICATIONS TO THIS FILE WILL BE OVERWRITTEN DURING GRADING
  */
@@ -11,7 +11,8 @@
 #include "utils.h"
 
 /* Performs some basic tests. Feel free to define additional tests. */
-void test_basic() {
+void test_basic()
+{
     Row *sparse_matrix;
 
     unsigned char small[] = {
@@ -28,6 +29,38 @@ void test_basic() {
 
     unsigned char tiny[] = { 56 };
 
+    unsigned char empty_center[] = {
+        0x01, 0x02, 0x03,
+        0x04, 0xFF, 0x06,
+        0x07, 0x08, 0x09
+    };
+
+    unsigned char white_margins_empty_spots[] = {
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0x01, 0x01, 0x01, 0x01, 0xFF,
+        0xFF, 0x02, 0x02, 0x02, 0x02, 0xFF,
+        0xFF, 0x03, 0xFF, 0xFF, 0x03, 0xFF,
+        0xFF, 0xFF, 0x04, 0x04, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+    };
+
+    unsigned char theres_a_seven_in_there[] = {
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0x07, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+    };
+
+    unsigned char skips_middle_line[] = {
+        0x01, 0x02, 0x03,
+        0xFF, 0xFF, 0xFF,
+        0x07, 0x08, 0x09
+    };
+
+
+
     sparse_matrix = dense_to_sparse(small, 3, 3);
     test_sparse("small", sparse_matrix);
     free_sparse(sparse_matrix);
@@ -39,11 +72,28 @@ void test_basic() {
     sparse_matrix = dense_to_sparse(tiny, 1, 1);
     test_sparse("tiny", sparse_matrix);
     free_sparse(sparse_matrix);
+
+    sparse_matrix = dense_to_sparse(empty_center, 3, 3);
+    test_sparse("empty_center", sparse_matrix);
+    free_sparse(sparse_matrix);
+
+    sparse_matrix = dense_to_sparse(white_margins_empty_spots, 6, 6);
+    test_sparse("white_margins_empty_spots", sparse_matrix);
+    free_sparse(sparse_matrix);
+
+    sparse_matrix = dense_to_sparse(theres_a_seven_in_there, 6, 6);
+    test_sparse("theres_a_seven_in_there", sparse_matrix);
+    free_sparse(sparse_matrix);
+
+    sparse_matrix = dense_to_sparse(skips_middle_line, 3, 3);
+    test_sparse("skips_middle_line", sparse_matrix);
+    free_sparse(sparse_matrix);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     Image img;
-    Row *sparse_matrix; 
+    Row *sparse_matrix;
 
     if ( argc != 2 ) {
         test_basic();
@@ -53,24 +103,25 @@ int main(int argc, char **argv) {
         sparse_matrix = dense_to_sparse(img.data, img.width, img.height);
         test_sparse(argv[1], sparse_matrix);
         free_sparse(sparse_matrix);
-		free(img.data);
+        free(img.data);
     }
     return 0;
 }
 
 /* Prints a sparse matrix. */
-void print_sparse(Row *sparse_matrix) {
+void print_sparse(Row *sparse_matrix)
+{
     Row *cur_row;
     Elem *cur_elem;
     int y;
 
     cur_row = sparse_matrix;
     printf("Printing sparse matrix:\n");
-    while(cur_row) {
+    while (cur_row) {
         y = cur_row->y;
         cur_elem = cur_row->elems;
         printf("Row %d: ", y);
-        while(cur_elem) {
+        while (cur_elem) {
             printf("[(%d,%d):%d] ", cur_elem->x, y, cur_elem->value);
             cur_elem = cur_elem->next;
         }
--- test_sparsify.c
+++ test_sparsify.c
@@ -1,6 +1,6 @@
 /* CS61C Sp14 Project 1
- * 
- * Contains test cases for sparsify. The test cases here are by no means 
+ *
+ * Contains test cases for sparsify. The test cases here are by no means
  * complete. You are HIGHLY encouraged to write your own tests.
  *
  * ANY MODIFICATIONS TO THIS FILE WILL BE OVERWRITTEN DURING GRADING
@@ -10,38 +10,54 @@
 #include <string.h>
 #include "sparsify.h"
 
-int check_sparse(Row *sparse_matrix, int *items, int max) {
+int check_sparse(Row *sparse_matrix, int *items, int max)
+{
     Row *cur_row;
     Elem *cur_elem;
 
     int cur = 0;
     cur_row = sparse_matrix;
-    while(cur_row) {
+    while (cur_row) {
         cur_elem = cur_row->elems;
-        while(cur_elem) {
-            if(items[cur] != (unsigned int) cur_elem->value) return 0;
+        while (cur_elem) {
+            if (items[cur] != (unsigned int) cur_elem->value) return 0;
             cur_elem = cur_elem->next;
             cur++;
-            if(cur>=max) return 0;
+            if (cur >= max) return 0;
         }
-        if(items[cur] != -1) return 0;
+        if (items[cur] != -1) return 0;
         cur_row = cur_row->next;
         cur++;
-        if(cur>=max) return 0;
+        if (cur >= max) return 0;
     }
-    return items[cur]==-2 ? 1 : 0;
+    return items[cur] == -2 ? 1 : 0;
 }
 
 
-void test_sparse(char *filename, Row *sparse_matrix) {
-    if(strcmp(filename, "small")==0) {
+void test_sparse(char *filename, Row *sparse_matrix)
+{
+    if (strcmp(filename, "small") == 0) {
         int items[] = {1, 2, 3, -1, 4, 5, 6, -1, 7, 8, 9, -1, -2};
-        printf("Result of small is %s\n", check_sparse(sparse_matrix, items, 13)==1? "correct" : "incorrect");
-    } else if (strcmp(filename, "empty")==0) {
-        int items[] = {-2};
-        printf("Result of empty is %s\n", check_sparse(sparse_matrix, items, 1)==1? "correct" : "incorrect");
-    } else if (strcmp(filename, "tiny")==0) {
+        printf("Result of small is %s\n", check_sparse(sparse_matrix, items, 13) == 1 ? "correct" : "incorrect");
+    } else if (strcmp(filename, "empty") == 0) {
+        int items[] = { -2};
+        printf("Result of empty is %s\n", check_sparse(sparse_matrix, items, 1) == 1 ? "correct" : "incorrect");
+    } else if (strcmp(filename, "tiny") == 0) {
         int items[] = {56, -1, -2};
-        printf("Result of tiny is %s\n", check_sparse(sparse_matrix, items, 3)==1? "correct" : "incorrect");
+        printf("Result of tiny is %s\n", check_sparse(sparse_matrix, items, 3) == 1 ? "correct" : "incorrect");
+    } else if (strcmp(filename, "empty_center") == 0) {
+        int items[] = {1, 2, 3, -1, 4, 6, -1, 7, 8, 9, -1, -2};
+        printf("Result of empty_center is %s\n", check_sparse(sparse_matrix, items, 13) == 1 ? "correct" : "incorrect");
+    } else if (strcmp(filename, "white_margins_empty_spots") == 0) {
+        int items[] = {1, 1, 1, 1, -1, 2, 2, 2, 2, -1, 3, 3, -1, 4, 4, -1, -2};
+        printf("Result of white_margins_empty_spots is %s\n", check_sparse(sparse_matrix, items, 39) == 1 ? "correct" : "incorrect");
+    } else if (strcmp(filename, "theres_a_seven_in_there") == 0) {
+        int items[] = {7, -1, -2};
+        printf("Result of theres_a_seven_in_there is %s\n", check_sparse(sparse_matrix, items, 3) == 1 ? "correct" : "incorrect");
+    } else if (strcmp(filename, "skips_middle_line") == 0) {
+        int items[] = {1, 2, 3, -1, 7, 8, 9, -1 - 2};
+        printf("Result of skips_middle_line is %s\n", check_sparse(sparse_matrix, items, 13) == 1 ? "correct" : "incorrect");
     }
+    //NOTE: -1: next row, -2: end of matrix
+    //      -1s do not seem to accumulate (ie one '-1' for skipping rows)
 }
